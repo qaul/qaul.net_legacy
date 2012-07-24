19d7bb5c79fcce7957aea325f0ee9df938dc486b
sojus <sojus@sojus>
2012-07-24 02:54:04 +0200

    Android voip compiling sucessful, still linking errors

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -56,44 +56,128 @@ LOCAL_CFLAGS := \
 	-I$(LOCAL_PATH)/../../pjproject_android/pjlib-util/include \
 	-I$(LOCAL_PATH)/../../pjproject_android/pjmedia/include \
 	-I$(LOCAL_PATH)/../../pjproject_android/pjnath/include
-
 LOCAL_STATIC_LIBRARIES := \
 	pjsip \
-	pjlib \
-	pjlib-util \
+    pjsip-simple \
+    pjsip-ua \
+    pjmedia-videdev \
+	pjmedia-codec \
 	pjmedia \
-	pjnath \
+	pjmedia-audiodev \
+	pjlib-util \
 	resample \
 	speex \
-	gsm 
-
-#LOCAL_LDLIBS += \
-#	-l/home/sojus/src/meshnet/qaul.net/android/obj/local/armeabi/libpjsip.a \
-#	-l/home/sojus/src/meshnet/qaul.net/android/obj/local/armeabi/libpjlib.a \
-#	-l/home/sojus/src/meshnet/qaul.net/android/obj/local/armeabi/libpjlib-util.a \
-#	-l/home/sojus/src/meshnet/qaul.net/android/obj/local/armeabi/libpjmedia.a \
-#	-l/home/sojus/src/meshnet/qaul.net/android/obj/local/armeabi/libpjnath.a \
-#	-l/home/sojus/src/meshnet/qaul.net/android/obj/local/armeabi/libresample.a \
-#	-l/home/sojus/src/meshnet/qaul.net/android/obj/local/armeabi/libspeex.a \
-#	-l/home/sojus/src/meshnet/qaul.net/android/obj/local/armeabi/libsrtp.a \
-#	-l/home/sojus/src/meshnet/qaul.net/android/obj/local/armeabi/libzrtp4pj.a \
-#	-l/home/sojus/src/meshnet/qaul.net/android/obj/local/armeabi/libgsm.a \
-#	-l/home/sojus/src/meshnet/qaul.net/android/obj/local/armeabi/libpj_g729_codec.a \
-#	-l/home/sojus/src/meshnet/qaul.net/android/obj/local/armeabi/libpj_amr_stagefright_codec.a
+    ilbccodec \
+    resample \
+    milenage \
+	gsmcodec \
+    g7221codec \
+    srtp \
+    pjnath \
+    pj \
+    pjsua 
 
 # add logging for debugging
 LOCAL_LDLIBS := \
-	-L$(SYSROOT)/usr/lib -llog 
+	-L$(SYSROOT)/usr/lib -llog \
+    
 include $(BUILD_SHARED_LIBRARY)
 
-# build sip libraries
-# Include submodules
-include $(JNI_PATH)/pjsip/android_toolchain/Android.mk
-include $(JNI_PATH)/silk/android_toolchain/Android.mk
-
+# include externally built libraries
+include $(CLEAR_VARS)
+LOCAL_MODULE            := pjsip 
+LOCAL_SRC_FILES         := ../../pjproject_android/pjsip/lib/libpjsip-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/pjsip/include
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := pjsip-simple
+LOCAL_SRC_FILES         := ../../pjproject_android/pjsip/lib/libpjsip-simple-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/pjsip/include
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := pjsip-ua 
+LOCAL_SRC_FILES         := ../../pjproject_android/pjsip/lib/libpjsip-ua-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/pjsip/include
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := pjsua 
+LOCAL_SRC_FILES         := ../../pjproject_android/pjsip/lib/libpjsua-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/pjsip/include
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := pjmedia-videodev 
+LOCAL_SRC_FILES         := ../../pjproject_android/pjmedia/lib/libpjmedia-videodev-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/pjmedia/include
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := pjmedia-codec 
+LOCAL_SRC_FILES         := ../../pjproject_android/pjmedia/lib/libpjmedia-codec-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/pjmedia/include
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := pjmedia
+LOCAL_SRC_FILES         := ../../pjproject_android/pjmedia/lib/libpjmedia-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/pjmedia/include
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := pjmedia-audiodev
+LOCAL_SRC_FILES         := ../../pjproject_android/pjmedia/lib/libpjmedia-audiodev-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/pjmedia/include
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := pjlib-util
+LOCAL_SRC_FILES         := ../../pjproject_android/pjlib-util/lib/libpjlib-util-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/pjlib-util/include
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := speex
+LOCAL_SRC_FILES         := ../../pjproject_android/third_party/lib/libspeex-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/third_party/include
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := ilbccodec
+LOCAL_SRC_FILES         := ../../pjproject_android/third_party/lib/libilbccodec-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/third_party/include
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := resample
+LOCAL_SRC_FILES         := ../../pjproject_android/third_party/lib/libresample-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/third_party/include
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := milenage
+LOCAL_SRC_FILES         := ../../pjproject_android/third_party/lib/libmilenage-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/third_party/include
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := gsmcodec
+LOCAL_SRC_FILES         := ../../pjproject_android/third_party/lib/libgsmcodec-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/third_party/include
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := g7221codec
+LOCAL_SRC_FILES         := ../../pjproject_android/third_party/lib/libg7221codec-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/third_party/include
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := srtp
+LOCAL_SRC_FILES         := ../../pjproject_android/third_party/lib/libsrtp-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/third_party/include
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := pjnath
+LOCAL_SRC_FILES         := ../../pjproject_android/pjnath/lib/libpjnath-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/pjnath/include
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := pj
+LOCAL_SRC_FILES         := ../../pjproject_android/pjlib/lib/libpj-arm-unknown-linux-androideabi.a
+LOCAL_EXPORT_C_INCLUDES := ../../pjproject_android/pjlib/include
+include $(PREBUILT_STATIC_LIBRARY)
 
 # libnativetask.so
 include $(CLEAR_VARS)
 LOCAL_MODULE    := libnativetask
 LOCAL_SRC_FILES := android_tether_system_NativeTask.c 
 include $(BUILD_SHARED_LIBRARY)
+
--- pjproject_android/pjlib/include/pj/config_site.h
+++ pjproject_android/pjlib/include/pj/config_site.h
@@ -1,3 +1,5 @@
 #define PJ_CONFIG_ANDROID 1
+#define PJ_IS_LITTLE_ENDIAN 1
+#define PJ_IS_BIG_ENDIAN 0
 #include <pj/config_site_sample.h>
 
