cd7c0cc8d97ab0f7f56a7c6cea1ccf6467b0678c
MathJud <jud@wachter-jud.net>
2012-08-28 07:47:34 +0200

    iOS: configuration works, still bug left (returning iphone screen after configuration)

--- ios/Classes/qaulAppDelegate.m
+++ ios/Classes/qaulAppDelegate.m
@@ -27,6 +27,10 @@
 	NSLog(@"initialize app");		
 	Qaullib_Init([qaulResourcePath UTF8String]);
 	
+	// set configuration
+	Qaullib_SetConf(QAUL_CONF_QUIT);
+	Qaullib_SetConf(QAUL_CONF_IOS);
+	
 	// start web server
 	NSLog(@"start web server");
 	if(Qaullib_WebserverStart())
@@ -136,181 +140,64 @@
 	// check authorization 
 	if(qaulStarted == 10)
 	{
-		// todo: how to get root previleges
-/*
-		// Create authorization reference
-		// authorization
-		NSLog(@"authorize");
-		status = AuthorizationCreate(NULL, kAuthorizationEmptyEnvironment, kAuthorizationFlagDefaults, &authorizationRef);	
-		if (status == errAuthorizationSuccess) NSLog(@"Authorization success");
-		else NSLog(@"Authorization failed");
-*/		
-		// use the authorization
-//		if([mysudo testAuthorization:authorizationRef]) NSLog(@"Authorization tested success");
-//		else NSLog(@"Authorization tested error");
+		// todo: how to get root previleges?
 		
 		qaulStarted = 20;
 	}
-/*	
-	// search Wifi interface
+	
+	
+	// wait for wifi configuration
 	if(qaulStarted == 20)
 	{
-		NSLog(@"loop through interfaces");
-		// -----------------------------------
-		// configure & start up wifi
-		qaulInterfacesAll = (NSArray *) SCNetworkInterfaceCopyAll ();
-		en = [qaulInterfacesAll objectEnumerator];
-		SCNetworkInterfaceRef inter;
-		SCPreferencesRef prefRef;
-		prefRef = SCPreferencesCreate(kCFAllocatorSystemDefault, (CFStringRef)@"XXX", NULL);
-		NSEnumerator *e = [(NSArray *)SCNetworkSetCopyServices(SCNetworkSetCopyCurrent(prefRef)) objectEnumerator];
-		SCNetworkServiceRef service;
-		
-		// loop through interfaces
-		while ((inter = (SCNetworkInterfaceRef) [en nextObject])) 
+		if(Qaullib_CheckConf(QAUL_CHECK_WIFI_SET))
 		{
-			//CFStringRef name = SCNetworkInterfaceGetBSDName (inter);
-			//NSLog(@"%@", name);
-			
-			// check if it is a Wifi
-			CFStringRef typesMy = SCNetworkInterfaceGetInterfaceType(inter);
-			if (typesMy == kSCNetworkInterfaceTypeIEEE80211)
-			{
-				//NSLog(@"Its a Wifi!");
-				qaulWifiInterface = inter;
-				qaulWifiInterfaceSet = TRUE;
-				// TODO:
-				// put connection into array
-				//[qaulInterfacesWifi addObject:[qaulInterfacesAll objectAtIndex:i]];
-				//[qaulInterfacesWifi addObject:inter];
-			}
+			NSLog(@"wifi configured");
+			qaulStarted = 25;
 		}
-		
-		// loop through Services
-		while (service = (SCNetworkServiceRef)[e nextObject]) 
+		else
 		{
-			inter = SCNetworkServiceGetInterface(service);
-			
-			// check if interface matches
-			if(CFEqual(qaulWifiInterface, inter))
-			{
-				qaulServiceId = service;
-				//CFStringRef name = SCNetworkInterfaceGetBSDName (inter);
-				//NSLog(@"Wifi Service found!");
-				//NSLog(@"%@", name);
-				
-				// enable it if disabled
-				if(!SCNetworkServiceGetEnabled(qaulServiceId))
-				{				
-					NSLog(@"enable Service");
-					if(!SCNetworkServiceSetEnabled(qaulServiceId, TRUE)) 
-						NSLog(@"service couldn't be enabled");
-					else qaulServiceConfigured = true;
-				}
-				else NSLog(@"SCNetworkServiceEnabled");
-				
-				// get Service Name
-				qaulServiceName = SCNetworkServiceGetName(qaulServiceId);
-				NSLog(@"service name: %@", qaulServiceName);
-				qaulServiceFound = TRUE;
-				break;
-			}
-		}
-		
-		//success = [mysudo startAirport:authorizationRef interface:qaulWifiInterface];
-		//if(success) NSLog(@"startAirport success!!");
-		//else NSLog(@"startAirport no success");
-		
-		//if(qaulServiceFound) ;
-		//else 
-		qaulStarted = 21;
-	}
-	
-	// enable Service
-	if(qaulStarted == 21)
-	{
-		
-		// check if the Service is enabled
-		if(!qaulServiceConfigured)
-		{
-			NSLog(@"Service not activated!");
-			
-			// TODO: create deleted Services
-			// create it
-			//SCNetworkSetAddService();
-			//service = SCNetworkServiceCreate (prefRef, inter);
-			// configure it
-			//[service release];
-			
-		}
-		
-		qaulStarted = 22;
-	}
-	
-	// switch airport on
-	if(qaulStarted == 22)
-	{
-		NSLog(@"switch airport on");
-		// switch on airport via cli 
-		success = [mysudo startAirport:authorizationRef interface:qaulWifiInterface];
-		if(success) NSLog(@"startAirport success!!");
-		else NSLog(@"startAirport no success");
-		
-		// set IP
-		NSString *myip = [NSString stringWithFormat:@"%s",Qaullib_GetIP()];
-		success = [mysudo setAddress:authorizationRef address:myip service:qaulServiceId];
-		//success = [mysudo setAddress:authorizationRef address:@"10.202.0.40" service:qaulServiceId];
-		if(success) NSLog(@"setAddress success!!");
-		else NSLog(@"setAddress no success");
-		
-		qaulStarted = 23;
-	}
-	
-	//23 usleep(1000000);
-	if (qaulStarted == 23)
-	{
-		if(floor(NSAppKitVersionNumber) > NSAppKitVersionNumber10_5)  [self startDelay: 15.0f];
-		else [self startDelay: 3.0f];
-	}
-	
-	// configure airport
-	if(qaulStarted == 24)
-	{
-		success = [mysudo connect2network:authorizationRef name:@"qaul.net" channel:11 interface:qaulWifiInterface];
-		if(success) NSLog(@"connect2network success!!");
-		else NSLog(@"connect2network no success");
-		
-		qaulStarted = 29;
-	}
-	
-	//39 usleep(7000000);
-	if(qaulStarted == 29) [self startDelay: 3.0f];
-	
-	// check username
-	if(qaulStarted == 30)
-	{
-		// wait until username is set
-		NSLog(@"check Username");
-		if(Qaullib_ExistsUsername()) qaulStarted = 40;
-		else 
-		{
-			qaulStarted = 29;
-			[self startDelay: 0.5f];
+			NSLog(@"wait for wifi configuration");
+			qaulStarted = 19;
+			[self startDelay: 1.0f];
 		}
 	}
 	
+	// set static ip
+	if(qaulStarted == 25)
+	{
+		// set static ip as root
+		
+		qaulStarted = 40;
+	}
+	
+		
 	// start olsrd
 	if(qaulStarted == 40)
 	{
-		success = [mysudo startOlsrd:authorizationRef interface:qaulWifiInterface];
-		if(success) NSLog(@"olsrd start success!!");
-		else NSLog(@"olsrd start no success");
+		NSLog(@"start olsrd");
+		
+		// retrieve current user id
+		uid_t uid;
+		uid = getuid();
+		
+		// set root rights
+		setuid(0);
+		system("/usr/bin/olsrd -i en0 -f /usr/bin/olsrd_ios.conf -d 0 -nofork");
+		
+		// set user back to original user id
+		setuid(uid);
+		
+		// check what user you are running under right now:
+		//system("whoami");
+		
+		NSLog(@"olsrd started");
 		
 		qaulStarted = 44;
 	}	
 	
-	//if(qaulStarted == 49) usleep(2000000);
-	if(qaulStarted == 44) [self startDelay: 2.0f];
+	if(qaulStarted == 44) 
+		[self startDelay: 2.0f];
+
 	
 	// connect ipc
 	if(qaulStarted == 45)
@@ -327,7 +214,7 @@
 		Qaullib_CaptiveStart();
 		
 		// start port forwarding
-		success = [mysudo startPortForwarding:authorizationRef interface:qaulWifiInterface];
+		//success = [mysudo startPortForwarding:authorizationRef interface:qaulWifiInterface];
 		
 		// TODO: configure firewall
 		
@@ -345,8 +232,138 @@
 		
 		qaulStarted = 60;
 	}
+}
+
+
+
+// -------------------------------------------------------------------------
+// Timer
+// -------------------------------------------------------------------------
+- (void)startTimer
+{
+	[qaullibTimer invalidate];
+    qaullibTimer = [NSTimer scheduledTimerWithTimeInterval: 0.1 target:self selector:
+					@selector(checkIpcMessage:) userInfo:nil repeats:YES];
+    [qaullibTimer fire];
+	
+	[qaullibTimer2 invalidate];
+    qaullibTimer2 = [NSTimer scheduledTimerWithTimeInterval: 3.0 target:self selector:
+					 @selector(checkIpcTopology:) userInfo:nil repeats:YES];
+    [qaullibTimer2 fire];
+	
+	[qaullibTimer3 invalidate];
+    qaullibTimer3 = [NSTimer scheduledTimerWithTimeInterval: 0.01 target:self selector:
+					 @selector(checkAppEvents:) userInfo:nil repeats:YES];
+    [qaullibTimer3 fire];
+}
+
+- (void)stopTimer
+{
+	[qaullibTimer invalidate];
+    qaullibTimer = nil;
+	
+	[qaullibTimer2 invalidate];
+    qaullibTimer2 = nil;
+	
+	[qaullibTimer3 invalidate];
+    qaullibTimer3 = nil;
+}
+
+- (void)checkIpcMessage:(NSTimer *)theTimer
+{
+	Qaullib_TimedSocketReceive();
+}
+
+- (void)checkIpcTopology:(NSTimer *)theTimer
+{
+	Qaullib_IpcSendCom(1);
+	Qaullib_TimedDownload();
+}
+
+- (void)checkAppEvents:(NSTimer *)theTimer
+{
+	int appEvent = Qaullib_TimedCheckAppEvent();
+/*
+	if (appEvent > 0)
+	{
+		NSLog([NSString stringWithFormat:@"appEvent found: %i\n", appEvent]);
+		
+		if(appEvent == QAUL_EVENT_CHOOSEFILE)
+		{
+			// show file picker
+			int i; // Loop counter.
+			
+			// Create the File Open Dialog class.
+			NSOpenPanel* openDlg = [NSOpenPanel openPanel];
+			
+			// Enable the selection of files in the dialog.
+			[openDlg setCanChooseFiles:YES];
+			
+			// Disable the selection of multiple files in the dialog.
+			[openDlg setAllowsMultipleSelection:NO];
+			
+			// Enable the selection of directories in the dialog.
+			//[openDlg setCanChooseDirectories:YES];
+			
+			// Display the dialog.  If the OK button was pressed,
+			// process the files.
+			if ( [openDlg runModalForDirectory:nil file:nil] == NSOKButton )
+			{
+				// Get an array containing the full filenames of all
+				// files and directories selected.
+				NSArray* files = [openDlg filenames];
+				
+				// Loop through all the files and process them.
+				for( i = 0; i < [files count]; i++ )
+				{
+					NSString* fileName = [files objectAtIndex:i];
+					
+					// Do something with the filename.
+					NSLog(@"filename: %@\n", fileName);
+					Qaullib_FilePicked(2, [fileName UTF8String]);
+				}
+			}			
+		}
+		else if(appEvent == QAUL_EVENT_OPENFILE)
+		{
+			// open file
+			[[NSWorkspace sharedWorkspace] openFile:[NSString stringWithFormat:@"%s", Qaullib_GetAppEventOpenPath()]];
+		}
+		else if(appEvent == QAUL_EVENT_OPENURL)
+		{
+			// open url
+			[[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"%s", Qaullib_GetAppEventOpenURL()]]];
+		}
+		else if(appEvent == QAUL_EVENT_QUIT)
+		{
+			[NSApp terminate:nil];
+		}
+		else if(appEvent == QAUL_EVENT_NOTIFY || appEvent == QAUL_EVENT_RING)
+		{
+			NSLog(@"QAUL_EVENT_NOTIFY or QAUL_EVENT_RING received\n");
+			NSBeep();
+		}
+	}
 */
 }
+
+
+// -------------------------------------------------------------------------
+// Wait Timer
+// -------------------------------------------------------------------------
+- (void)startDelay:(NSTimeInterval)secs 
+{
+	NSLog(@"start Delay %f s\n",secs);
+	[self performSelector:@selector(fireDelay) withObject:nil afterDelay:secs];	
+}
+
+- (void)fireDelay
+{
+	NSLog(@"Delay fired\n");
+	qaulStarted++;
+	[self QaulConfigure];
+}
+
 
 #pragma mark -
 #pragma mark Memory management
