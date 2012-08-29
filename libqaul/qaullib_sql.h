b01586b543aaa69cd611f8f5ba4c6fa51ee38ac1
MathJud <jud@wachter-jud.net>
2012-08-29 12:28:31 +0200

    OSX: DMG installer for osx

--- libqaul/qaullib_sql.h
+++ libqaul/qaullib_sql.h
@@ -188,7 +188,7 @@ struct qaul_populate_file_struct
 static struct qaul_populate_file_struct qaul_populate_file[MAX_POPULATE_FILE] = {
 	{1,  4, 8433817, "7512bc3b516cda638efb2b4e69f2dbf021413c3a", "gz",  "ubuntu & debian 32 Bit",10000000},
 //	{2,  4, 2068596, "0000000000000000000000000000000000000000", "zip", "OSX 10.5",               4000000},
-	{4,  4, 2068596, "0a731b0bff9c21a74d17eb2363f1aced96ebe257", "zip", "OSX 10.6",               4000000},
+	{4,  4, 2068596, "0a731b0bff9c21a74d17eb2363f1aced96ebe257", "dmg", "OSX 10.6",               6000000},
 	{8,  4, 4151214, "f26c547e84baf2559803d783de2f389718665580", "exe", "Windows 7",              7000000},
 	{16, 4, 2085071, "f57b66db6fc0b5f0090d54cbe4d45739970ea3d3", "apk", "Android",                4000000}
 };
