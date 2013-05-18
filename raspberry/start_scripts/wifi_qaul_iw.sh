3da76001e083c54672c000c4dbb968763f7cb9a6
sojus <sojus@sojus>
2013-05-18 21:37:35 +0200

    Raspberry: start with iw

---
+++ raspberry/start_scripts/wifi_qaul_iw.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# usage:
+## wifi_qaul_iw.sh <INTERFACE> <IP>
+## wifi_qaul_iw.sh wlan0 10.111.111.1
+
+# put card into ibss mode
+sudo iw dev $1 set type ibss
+
+# create or join network
+# channel 1 : 2412
+# channel 11: 2462
+sudo iw dev $1  ibss join 2462
+
+sudo ifconfig $1 $2 netmask 255.0.0.0 broadcast 255.255.255.255
+
+sleep 2
+
