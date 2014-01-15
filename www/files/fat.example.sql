bcf44194b18703f7a95cddace0cad00a49c32b76
MathJud <jud@wachter-jud.net>
2014-01-15 17:44:54 +0100

    Qaullib: Major Bugfix: Fat client template: don't enter NULL for integer -> System crashes

--- www/files/fat.example.sql
+++ www/files/fat.example.sql
@@ -1,7 +1,7 @@
-INSERT INTO "config" ('key','type','value','value_int') VALUES('exe.1',1,'0ac217905eb913647bfa9e14ff81f13fc127ed0c',NULL);
-INSERT INTO "config" ('key','type','value','value_int') VALUES('exe.4',1,'c11fbd4a4fa33020eb8bc4a913a7261de404db39',NULL);
-INSERT INTO "config" ('key','type','value','value_int') VALUES('exe.8',1,'b3043c6abf83dcc8ab86fb3a40999df96bf8b0fd',NULL);
-INSERT INTO "config" ('key','type','value','value_int') VALUES('exe.16',1,'efc8af6564a0afd7981c04a980514d9a086a95df',NULL);
+INSERT INTO "config" ('key','type','value') VALUES('exe.1',1,'0ac217905eb913647bfa9e14ff81f13fc127ed0c');
+INSERT INTO "config" ('key','type','value') VALUES('exe.4',1,'c11fbd4a4fa33020eb8bc4a913a7261de404db39');
+INSERT INTO "config" ('key','type','value') VALUES('exe.8',1,'b3043c6abf83dcc8ab86fb3a40999df96bf8b0fd');
+INSERT INTO "config" ('key','type','value') VALUES('exe.16',1,'efc8af6564a0afd7981c04a980514d9a086a95df');
 
 INSERT INTO "file" ('type','hash','suffix','description','size','status','created_at') VALUES(4,'0ac217905eb913647bfa9e14ff81f13fc127ed0c','gz','ubuntu & debian 32 Bit',9486792,5,1365582282);
 INSERT INTO "file" ('type','hash','suffix','description','size','status','created_at') VALUES(4,'c11fbd4a4fa33020eb8bc4a913a7261de404db39','dmg','OSX 10.6 - 10.9',4555287,5,1365582282);
