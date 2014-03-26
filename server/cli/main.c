8e68c5b5d2e296b7ede0ceb013cf70cc00d11922
MathJud <jud@wachter-jud.net>
2014-03-26 06:07:21 +0100

    Server: cli: bugfix: set QaulServerIP

--- server/cli/main.c
+++ server/cli/main.c
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
 	if (!Qaullib_DbGetConfigValue("ip", qaul_ip_str))
 	{
 		// set IP
-		Qaullib_SetIP(qaul_ip_str);
+		Qaullib_SetIP(QaulServerIP);
 	}
 	// load the IP otherwise
 	printf("IP: %s\n", Qaullib_GetIP());
