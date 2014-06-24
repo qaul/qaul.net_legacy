a40b0b6d31b2425bdbed0219e35cf815a0a6e481
pix <pix@pix-debian7.pix>
2014-06-24 22:00:55 +0200

    Linux: Fat client hash values for deb files.

--- www/files/fat.example.sql
+++ www/files/fat.example.sql
@@ -1,5 +1,5 @@
 INSERT INTO "config" ('key','type','value') VALUES('exe.4',1,'259d23a926de79897c69613bad81abd3c1ddda80');
-INSERT INTO "file" ('type','hash','suffix','description','size','status','created_at') VALUES(4,'259d23a926de79897c69613bad81abd3c1ddda80','dmg','OSX 10.7 - 10.9',8046519,5,1389912303);
+INSERT INTO "file" ('type','hash','suffix','description','size','status','created_at') VALUES(4,'259d23a926de79897c69613bad81abd3c1ddda80','dmg','OSX 10.6 - 10.9',8046519,5,1389912303);
 
 INSERT INTO "config" ('key','type','value') VALUES('exe.8',1,'68655ebdbc03dda9898a6aa7acfbe0946bef981a');
 INSERT INTO "file" ('type','hash','suffix','description','size','status','created_at') VALUES(4,'68655ebdbc03dda9898a6aa7acfbe0946bef981a','exe','Windows 7 / 8',4971151,5,1389912303);
@@ -7,8 +7,8 @@ INSERT INTO "file" ('type','hash','suffix','description','size','status','create
 INSERT INTO "config" ('key','type','value') VALUES('exe.16',1,'78f6362772319c2a0211a517480e30d6f138d5df');
 INSERT INTO "file" ('type','hash','suffix','description','size','status','created_at') VALUES(4,'78f6362772319c2a0211a517480e30d6f138d5df','apk','Android',3087953,5,1389912303);
 
-INSERT INTO "config" ('key','type','value') VALUES('exe.64',1,'2995fec880c93813d2500b83585c4178320888d9');
-INSERT INTO "file" ('type','hash','suffix','description','size','status','created_at') VALUES(4,'2995fec880c93813d2500b83585c4178320888d9','deb','ubuntu & debian 32 Bit',3120554,5,1403191947);
+INSERT INTO "config" ('key','type','value') VALUES('exe.64',1,'05a91f31a38393b84ebb07f3aa0dd1a98164d950');
+INSERT INTO "file" ('type','hash','suffix','description','size','status','created_at') VALUES(4,'05a91f31a38393b84ebb07f3aa0dd1a98164d950','deb','ubuntu & debian 32 Bit',2632436,5,1403606801);
 
-INSERT INTO "config" ('key','type','value') VALUES('exe.128',1,'2995fec880c93813d2500b83585c4178320888d9');
-INSERT INTO "file" ('type','hash','suffix','description','size','status','created_at') VALUES(4,'2995fec880c93813d2500b83585c4178320888d9','deb','ubuntu & debian 64 Bit',3120554,5,1403191947);
+INSERT INTO "config" ('key','type','value') VALUES('exe.128',1,'5c20b5d1aff1e1d8ace1de243bc6c604d6a76c4a');
+INSERT INTO "file" ('type','hash','suffix','description','size','status','created_at') VALUES(4,'5c20b5d1aff1e1d8ace1de243bc6c604d6a76c4a','deb','ubuntu & debian 64 Bit',2683786,5,1403606774);
