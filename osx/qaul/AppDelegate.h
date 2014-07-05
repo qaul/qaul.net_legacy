251e730538281752e1c99973ffc975e7ee68663e
MathJud <jud@wachter-jud.net>
2014-07-05 11:58:14 +0200

    OSX: new version for OSX 10.6-10.9

--- osx/qaul/AppDelegate.h
+++ osx/qaul/AppDelegate.h
@@ -4,22 +4,22 @@
  */
 
 #import <Cocoa/Cocoa.h>
-#import <WebKit/WebView.h>
+#import <WebKit/WebKit.h>
 #import "QaulConfigWifi.h"
 
 
-@interface WindowController : NSObject /* Specify a superclass (eg: NSObject or NSView) */ {
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+{
+    NSWindow *qaulWindow;
+    WebView *qaulWebView;
+    
     IBOutlet id start_name;
-    IBOutlet id qaulTabView;
-	IBOutlet WebView *qaulWebView;
 	NSString *username;
 	NSString *qaulResourcePath;
 	int qaulStarted;
 	
 	// wifi config
 	QaulConfigWifi *qaulConfigWifi;
-//	NSArray * qaulInterfacesAll;
-//	NSEnumerator *en;
     BOOL qaulInterfaceManual;
     NSString *qaulInterfaceName;
 	SCNetworkInterfaceRef qaulWifiInterface;
@@ -30,7 +30,6 @@
 	BOOL qaulServiceConfigured;
 	CFStringRef qaulServiceName;
 	OSStatus status;
-//	AuthorizationRef authorizationRef;
 	
 	// Timer
 	NSTimer *qaullibTimer;
@@ -38,29 +37,19 @@
 	NSTimer *qaullibTimer3;
 }
 
+@property (assign) IBOutlet NSWindow *qaulWindow;
+@property (assign) IBOutlet WebView *qaulWebView;
+
 /**
  * copy files to the home directory the first time the application is started
  */
 - (void)copyFilesAtFirstStartup;
 - (void)createDirectory:(NSString *)directoryName atFilePath:(NSString *)filePath;
 
-/**
- * window loaded
- */
-- (void)awakeFromNib;
-- (void)applicationDidFinishLaunching:(id)sender;
-
-/**
- * window is closing
- */
-- (BOOL)windowShouldClose:(id)sender;
-- (BOOL)applicationShouldTerminate:(id)app;
-
 /** 
  * support methods
  */
 - (void)endAlert:(id)sheet returnCode:(int)returnCode contextInfo:(void *)contextInfo;
-
 - (void)init_app;
 
 /**
