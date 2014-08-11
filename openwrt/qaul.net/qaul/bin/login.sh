7e17b13d2610df05f3234366e69d79d23d4d4bf6
MathJud <jud@wachter-jud.net>
2014-08-11 15:42:46 +0200

    openwrt: web admin: check and store authentication

--- openwrt/qaul.net/qaul/bin/login.sh
+++ openwrt/qaul.net/qaul/bin/login.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 ## login user
+## usage:
+##     ./login.sh <PASSWORD> <SESSIONHASH> <IP>
 
+# check if password is valid
+
+# create session 
+echo "$3" > "/qaul/auth/$2"
+
+echo "1"
 
 exit 0
--- openwrt/qaul.net/qaul/bin/logout.sh
+++ openwrt/qaul.net/qaul/bin/logout.sh
@@ -2,4 +2,10 @@
 
 ## logout user
 
+IFS="&="
+set -- $HTTP_COOKIE
+MD5STRING=$2
+
+rm /qaul/auth/$MD5STRING
+
 exit 0
