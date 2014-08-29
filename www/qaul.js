949df348bc86102c0ff080f45a31aae316b67ee3
MathJud <jud@wachter-jud.net>
2014-08-29 14:01:26 +0200

    GUI: fix local time error in chat messages

--- www/qaul.js
+++ www/qaul.js
@@ -1363,12 +1363,12 @@ function isoDateString(d)
 {
 	function pad(n){return n<10 ? '0'+n : n};
 	
-	return d.getUTCFullYear()+'-'
-      + pad(d.getUTCMonth()+1)+'-'
-      + pad(d.getUTCDate())+'T'
-      + pad(d.getUTCHours())+':'
-      + pad(d.getUTCMinutes())+':'
-      + pad(d.getUTCSeconds())+'Z';
+	return d.getFullYear()+'-'
+      + pad(d.getMonth()+1)+'-'
+      + pad(d.getDate())+'T'
+      + pad(d.getHours())+':'
+      + pad(d.getMinutes())+':'
+      + pad(d.getSeconds())+'Z';
 }
 
 // ======================================================
